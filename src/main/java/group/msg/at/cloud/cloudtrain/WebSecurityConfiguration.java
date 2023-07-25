0bf6fc853f0e87c4369aa23c7df27783622598f6
Michael Theis (msg) <michael.theis@msg.group>
2023-07-25 18:48:31 +0200

    got rid of obsolete imports breaking the build

--- src/main/java/group/msg/at/cloud/cloudtrain/WebSecurityConfiguration.java
+++ src/main/java/group/msg/at/cloud/cloudtrain/WebSecurityConfiguration.java
@@ -1,7 +1,5 @@
 package group.msg.at.cloud.cloudtrain;
 
-import group.msg.at.cloud.common.security.oidc.resource.server.OidcMappedJwtClaimSetConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
